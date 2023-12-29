dd19c38f574fa7146690436b0adaa9dcc85792b8
Rohith <rohithachar2000@gamil.com>
2023-12-29 10:49:13 +0530

    fixed checkWinner() and logic

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const Gameboard = {
     remainingSlot : 9,
     board : new Array(9),
     init : function(){
-        this.board = [];
+        this.board = new Array(9);
         this.remainingSlot = 9;
+        this.bindEvent();
     },
     addX : function(index){
         if(this.remainingSlot > 0){
@@ -29,22 +30,18 @@ const Gameboard = {
     checkWinner : function(){
         for (const [index,ele] of this.board.entries()) {
             if(ele === "X" || ele === "O"){
-                if(index <= 6 && this.board[index] === this.board[index + 1] &&
+                if(this.board[index] === this.board[index + 1] &&
                     this.board[index] === this.board[index + 2]){
-                        this.init();
                         return ele;
                 }
-                if(index <= 2 && this.board[index] === this.board[index + 3] &&
+                if(this.board[index] === this.board[index + 3] &&
                     this.board[index] === this.board[index + 6]){
-                        this.init();
                         return ele;
                 }
-                if(this.board[0] === this.board[4] && this.board[0] === this.board[8]){
-                    this.init();
+                if(this.board[index] === this.board[index + 4] && this.board[index] === this.board[index + 8]){
                     return ele;
                 }
-                if(this.board[2] === this.board[4] && this.board[2] === this.board[6]){
-                    this.init();
+                if(this.board[index] === this.board[index + 2] && this.board[index] === this.board[index + 4]){
                     return ele;
                 }
             }
@@ -52,7 +49,6 @@ const Gameboard = {
     }
 };
 Gameboard.init();
-Gameboard.bindEvent();
 function play(event){
     if(Gameboard.remainingSlot >= 0){
         userInput = event.target.id;
@@ -63,13 +59,17 @@ function play(event){
         if(Gameboard.remainingSlot != 0){
             var randomIndex = Math.floor(Math.random()*9);
             while(Gameboard.board[randomIndex] != null){
-                console.log("Hi");
                 randomIndex = Math.floor(Math.random()*9);
             }
         }
         Gameboard.addO(randomIndex);
+        console.log(Gameboard.board);
         if(Gameboard.checkWinner() === "X" || Gameboard.checkWinner === "O"){
             console.log("Winner = "+Gameboard.checkWinner());
+            Gameboard.init();
+        }
+        if(Gameboard.remainingSlot <= 0){
+            Gameboard.init();
         }
     } 
 }
